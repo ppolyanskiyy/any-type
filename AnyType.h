f77487f8d5c5cb8b33767d9659ea3f901ddd6595
Andrew Polyanskiy <71780594+ppolyanskiyy@users.noreply.github.com>
2020-10-14 15:00:48 +0300

    Update AnyType.h

--- AnyType.h
+++ AnyType.h
@@ -1,37 +1,30 @@
 #pragma once
-#include<iostream>
-#include<string>
-#include<variant>
-#include<algorithm>
+#include <iostream>
+#include <string>
+#include <variant>
+#include <algorithm>
 
 using namespace std;
 
-enum DataIndex { DATA_BEGIN = -1, CHAR_DATA, BOOL_DATA, INT_DATA, DOUBLE_DATA, DATA_END };
+enum DataIndex
+{
+	DATA_BEGIN = -1,
+	CHAR_DATA, BOOL_DATA, INT_DATA, DOUBLE_DATA,
+	LONG_INT_DATA, LONG_DOUBLE_DATA,
+	DATA_END
+};
 
-using StandartTypes = variant<char, bool, int, double>;
+using StandartTypes = variant<char, bool, int, double, long int, long double>;
 
-class AnyType {
-private:
-	StandartTypes m_data;
-	
-	inline const int get_index(const string& str)const {
-		if ("char" == str)return CHAR_DATA;
-		else if ("bool" == str)return BOOL_DATA;
-		else if ("int" == str)return INT_DATA;
-		else if ("double" == str)return DOUBLE_DATA;
-		else return DATA_BEGIN;
-	}
-
+class AnyType
+{
 public:
-
 	AnyType() = default;
 	AnyType(StandartTypes);
 	AnyType(string) = delete;
 
 	AnyType(const AnyType&);
 	AnyType(const AnyType&&) = delete;
-
-	bool operator==(const AnyType&);
 
 	template<typename Type>
 	AnyType& operator=(const Type&);
@@ -41,35 +34,36 @@ public:
 	string get_type()const;
 
 	template<typename Type>
-	Type& extract_value()const;
+	Type extract_value()const;
 
 	void swap(AnyType&);
+
+private:
+	const int get_index(const string&)const;
+
+	StandartTypes m_data;
 };
 
 // constructors
 AnyType::AnyType(StandartTypes data) : m_data(data) {}
 AnyType::AnyType(const AnyType& other) : m_data(other.m_data) {};
 
-// operator==
-inline bool AnyType::operator==(const AnyType& other) {
-	return this->m_data == other.m_data;
-};
-
 // operator=
 template<typename Type>
-inline AnyType& AnyType::operator=(const Type& other) {
+AnyType& AnyType::operator=(const Type& other)
+{
 	int index = get_index(typeid(other).name());
 
-	if (DATA_BEGIN < index && index < DATA_END) {
+	if (DATA_BEGIN < index && index < DATA_END)
+	{
 		this->m_data = other;
 		return *this;
 	}
-	else {
-		throw bad_cast();
-	}
+	else throw bad_cast();
 }
 
-inline AnyType& AnyType::operator=(const AnyType& other) {
+AnyType& AnyType::operator=(const AnyType& other)
+{
 	if (this->m_data == other.m_data) return *this;
 
 	this->m_data = other.m_data;
@@ -77,31 +71,40 @@ inline AnyType& AnyType::operator=(const AnyType& other) {
 }
 
 // Return type of data
-inline string AnyType::get_type() const {
-	string type = visit([](auto const& data) {
-		return typeid(data).name();
+string AnyType::get_type() const
+{
+	string type = visit([](auto const& data)
+		{
+			return typeid(data).name();
 		}, this->m_data);
 
 	return type;
 }
 
-// Extracts value from variant 
+// Extracts value from variant, throw bad variant access
 template<typename Type>
-inline Type& AnyType::extract_value() const {
-	Type result{};
-
-	if (typeid(result) == typeid(Type)) {
-		result = get<Type>(m_data);
-		return result;
-	}
-	else {
-		throw bad_cast();
-	}
+Type AnyType::extract_value() const
+{
+	Type result = get<Type>(m_data);
+	return result;
 }
 
 // Swap two objects
-inline void AnyType::swap(AnyType& other) {
+void AnyType::swap(AnyType& other)
+{
 	StandartTypes to_swap = this->m_data;
 	this->m_data = other.m_data;
 	other.m_data = to_swap;
+}
+
+// Return index for string
+const int AnyType::get_index(const string& str) const
+{
+	if ("char" == str)return CHAR_DATA;
+	else if ("bool" == str)return BOOL_DATA;
+	else if ("int" == str)return INT_DATA;
+	else if ("double" == str)return DOUBLE_DATA;
+	else if ("long int" == str)return LONG_INT_DATA;
+	else if ("long double" == str) return LONG_DOUBLE_DATA;
+	else return DATA_BEGIN;
 }
